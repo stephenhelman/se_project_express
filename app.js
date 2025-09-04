318d9aedca24bd390302a27de008707815608192
Stephen Helman <stephenhelman18@gmail.com>
2025-09-04 11:47:32 -0600

    move user id auth

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const { addUserToRequest } = require("./middleware/auth");
-
 const { PORT = 3001 } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(addUserToRequest);
+app.use((req, res, next) => {
+  req.user = {
+    _id: "68ab9c6e4c4145949ba823ea", // paste the _id of the test user created in the previous step
+  };
+  next();
+});
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
